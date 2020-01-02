6bf5cef1206804ce88a98c0fc84ae10386dbee01
Zeemah <jhodgson1@protonmail.ch>
2020-01-02 03:32:16 +0000

    dev: root changes

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,10 +1,12 @@
 import * as colors from 'colors';
-import { TextChannel } from 'discord.js';
+// import { TextChannel, Message } from 'discord.js';
 import { CommandoClient } from 'discord.js-commando';
 import { join } from 'path';
-import { version } from 'typescript';
+// import { version } from 'typescript';
 import 'typescript';
 import './lib/env';
+import commandExecution from './listeners/commandExecution';
+import messageHandling from './listeners/message';
 
 colors.setTheme({
     debug: 'cyan',
@@ -13,7 +15,7 @@ colors.setTheme({
     warn: 'yellow'
 });
 
-const prefix = 'st!';
+const prefix = process.env.PREFIX ?? 'st!';
 export const client = new CommandoClient({
     commandPrefix: prefix,
     invite: 'https://discord.gg/SbfA2Sf',
@@ -27,13 +29,15 @@ client
         console.log(`Logged in as ${client.user?.tag}! (${client.user?.id})`.green);
         console.log(`Prefix is set to: ${prefix}`.cyan);
     })
+    .on('message', messageHandling)
+    .on('commandRun', commandExecution)
     .registry
         .registerDefaultTypes()
         .registerGroups([
             ['misc', 'Miscellaneous commands that don\'t fit in other groups.'],
             ['information', 'Commands that provide useful information to the user.'],
             ['admin', 'Commands to help administration give out information and perform their tasks more easily.'],
-            ['fivem', 'Commands that are related to FiveM.']
+            // ['fivem', 'Commands that are related to FiveM.']
         ])
         .registerDefaultGroups()
         .registerDefaultCommands({
