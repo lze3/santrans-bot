29e4c4552c0b50515112ca147f45ebf546ce2849
Zeemah <jhodgson1@protonmail.ch>
2020-01-05 16:14:04 +0000

    feat(function): capitalize function

--- src/bot/util/functions.ts
+++ src/bot/util/functions.ts
@@ -1,1 +1,5 @@
 export const wait: any = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export function capitalize(init_str: string): string {
+    return init_str.charAt(0).toUpperCase() + init_str.slice(1);
+}
