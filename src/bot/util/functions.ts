aaff8b2411d5247b390d13d98004e1158d80b68a
Zeemah <jhodgson1@protonmail.ch>
2020-01-08 23:23:51 +0000

    dev(util): export SanTrans icon

--- src/bot/util/functions.ts
+++ src/bot/util/functions.ts
@@ -3,3 +3,5 @@ export const wait: any = (ms: number) => new Promise(res => setTimeout(res, ms))
 export function capitalize(init_str: string): string {
     return init_str.charAt(0).toUpperCase() + init_str.slice(1);
 }
+
+export const st_url: string = 'https://i.imgur.com/Q4Uibii.png';
